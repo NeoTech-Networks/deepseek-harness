83f763c5552ced8d7104d853456e27c5f755e86c
neotechnet <steve@neotechnetworks.com>
2026-09-08 17:58:09 -0400

    fix(win32-process): spawn console subprocesses without a visible window

--- packages/subprocess/win32-process/src/process.ts
+++ packages/subprocess/win32-process/src/process.ts
@@ -531,7 +531,7 @@ export function spawnCurrentTokenJobProcess(
       null,
       null,
       1,
-      abi.CREATE_SUSPENDED | abi.CREATE_UNICODE_ENVIRONMENT,
+      abi.CREATE_SUSPENDED | abi.CREATE_UNICODE_ENVIRONMENT | abi.CREATE_NO_WINDOW,
       environment,
       options.cwd,
       startupInfo,
